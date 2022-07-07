4e5e17307edf86aaa9da8bd52dc94b412863d894
caipirginka <github@oicom.com>
2022-07-07 15:21:31 +0200

    Also mount volume "amat-osmtools-script" under "/amat-osmtools/script" path.

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 
-docker run --network host --env-file %1 --rm -v amat-osmtools-data:/amat-osmtools-data amat-osmtools:2.2.1 %2 %3 %4 %5 %6 %7 %8 %9
+docker run --network host --env-file %1 --rm -v amat-osmtools-data:/amat-osmtools-data -v amat-osmtools-script:/amat-osmtools/script amat-osmtools:2.2.1 %2 %3 %4 %5 %6 %7 %8 %9
