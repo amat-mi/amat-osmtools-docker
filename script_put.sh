027bd0a52a38889dfd6ecf3bbb7967d13a5ad0f8
caipirginka <github@oicom.com>
2022-07-07 15:22:15 +0200

    Created "script_put" script to copy any custom script into container.

---
+++ script_put.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE_PUT_SRC=$1
+FILE_PUT_DST=$2
+
+docker container create -t --name amat-osmtools-dummy -v amat-osmtools-script:/amat-osmtools/script amat-osmtools:2.2.1 /bin/bash
+docker cp $FILE_PUT_SRC amat-osmtools-dummy:/amat-osmtools/script/$FILE_PUT_DST
+docker start amat-osmtools-dummy
+docker exec amat-osmtools-dummy chmod ug+x,o= /amat-osmtools/script/$FILE_PUT_DST
+docker exec amat-osmtools-dummy dos2unix /amat-osmtools/script/$FILE_PUT_DST
+docker stop amat-osmtools-dummy
+docker rm amat-osmtools-dummy
